85df87e26fd998881e077cb5b8a5b6525f20d465
dhruvpatel21121118 <dhruvkukarvadiya1118@gmail.com>
2026-06-04 10:56:53 +0530

    Update u1_8_1.php

--- u1_8_1.php
+++ u1_8_1.php
@@ -10,8 +10,30 @@
     $i =array('cold','hot','rain');
     foreach($i as $d){
         echo $d."<br>";
-        
     }
+$person = [
+    "name" => "John",
+    "age" => 25
+];
+
+echo "Name: " . $person["name"] . "<br>";
+echo "Age: " . $person["age"];
+
+
+$students = [
+    ["John", 85, "A"],
+    ["Alice", 90, "A+"],
+    ["Bob", 78, "B"]
+];
+
+
+echo $students[0][0]; // John
+echo "<br>";
+echo $students[1][1]; // 90
+echo "<br>";
+echo $students[2][2]; // B
+
+    
     ?>
 </body>
 </html>
